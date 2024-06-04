9b22bd969a23f78965f80d7da3d1c745ba29618b
Damon <chaoming@manta.network>
2024-06-04 16:00:22 +0800

    alias use @

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite'
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"), // import { ... } from "@/..."
+      },
+    },
+    plugins: [react()],
+    define: {
+      __APP_ENV__: env
+    },
+  };
+});
