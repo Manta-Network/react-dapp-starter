eed2c455bba73826827110d4eeabeece2986bf04
jason <jasonintju@gmail.com>
2024-07-11 00:04:42 +0800

    add @web3modal/wagmi

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,48 @@ import ReactDOM from 'react-dom/client';
 import AppRouter from './AppRouter.tsx';
 import './index.css';
 
+import { createWeb3Modal } from '@web3modal/wagmi/react';
+import { defaultWagmiConfig } from '@web3modal/wagmi/react/config';
+
+import { WagmiProvider } from 'wagmi';
+import { arbitrum, mainnet } from 'wagmi/chains';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+// 0. Setup queryClient
+const queryClient = new QueryClient();
+
+// 1. Get projectId from https://cloud.walletconnect.com
+const projectId = 'YOUR_PROJECT_ID';
+
+// 2. Create wagmiConfig
+const metadata = {
+	name: 'React Dapp Starter',
+	description: 'React Dapp Starter Example',
+	url: 'https://cedefi.manta.network',
+	icons: ['https://cedefi.manta.network/favicon.ico'],
+};
+
+const chains = [mainnet, arbitrum] as const;
+const config = defaultWagmiConfig({
+	chains,
+	projectId,
+	metadata,
+});
+
+// 3. Create modal
+createWeb3Modal({
+	wagmiConfig: config,
+	projectId,
+	enableAnalytics: true, // Optional - defaults to your Cloud configuration
+	enableOnramp: true, // Optional - false as default
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<AppRouter />
+		<WagmiProvider config={config}>
+			<QueryClientProvider client={queryClient}>
+				<AppRouter />
+			</QueryClientProvider>
+		</WagmiProvider>
 	</React.StrictMode>
 );
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,31 @@
+import { useWeb3Modal } from '@web3modal/wagmi/react';
+import { useAccount, useDisconnect } from 'wagmi';
+
 function Home() {
-	return <div>Home Page</div>;
+	const { open } = useWeb3Modal();
+	const { address } = useAccount();
+	const { disconnect } = useDisconnect();
+	return (
+		<div>
+			Home Page
+			{address ? (
+				<button
+					onClick={() => disconnect()}
+					className="block bg-blue-400 text-white py-2 px-4 rounded-md"
+				>
+					Disconnect
+				</button>
+			) : (
+				<button
+					onClick={() => open()}
+					className="block bg-blue-400 text-white py-2 px-4 rounded-md"
+				>
+					Connect Wallet
+				</button>
+			)}
+			{address && `Connected as ${address}`}
+		</div>
+	);
 }
 
 export default Home;
