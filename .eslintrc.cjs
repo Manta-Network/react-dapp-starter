aabd1e5f6ac4fcbf4d4cfda5af7afc7ba05275f0
jason <jasonintju@gmail.com>
2024-07-25 21:28:29 +0800

    set up shadcn-ui

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -19,6 +19,7 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'react/prop-types': 'off',
     'import/order': [
       'error',
       {
