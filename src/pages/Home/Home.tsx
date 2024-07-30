1700b21e4e72013ffec87b4dce796b98580679cf
jason <jasonintju@gmail.com>
2024-07-30 13:27:40 +0800

    add shadcn/ui checkbox

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -11,6 +11,7 @@ import {
   DialogTrigger,
 } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
+import { Checkbox } from '@/components/ui/checkbox';
 
 function Home() {
   const { open } = useWeb3Modal();
@@ -29,8 +30,22 @@ function Home() {
         <Button onClick={() => open()}>Connect Wallet</Button>
       )}
       {address && `Connected as ${address}`}
-      <div>
+      <div className="my-4">
         <Button onClick={showErrorToast}>error toast</Button>
+      </div>
+      <div className="items-top my-4 flex space-x-2">
+        <Checkbox id="terms1" />
+        <div className="grid gap-1.5 leading-none">
+          <label
+            htmlFor="terms1"
+            className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+          >
+            Accept terms and conditions
+          </label>
+          <p className="text-sm text-muted-foreground">
+            You agree to our Terms of Service and Privacy Policy.
+          </p>
+        </div>
       </div>
       <div>
         <Dialog>
