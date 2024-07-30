01703e4c152ddcec9c706e3a6df614ebf1210e90
jason <jasonintju@gmail.com>
2024-07-30 12:46:14 +0800

    add shadcn/ui dialog

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,6 +2,14 @@ import { useWeb3Modal } from '@web3modal/wagmi/react';
 import { useAccount, useDisconnect } from 'wagmi';
 import { toast } from 'sonner';
 import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
 
 function Home() {
   const { open } = useWeb3Modal();
@@ -22,6 +30,22 @@ function Home() {
       {address && `Connected as ${address}`}
       <div>
         <Button onClick={showErrorToast}>error toast</Button>
+      </div>
+      <div>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline">Edit Profile</Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Edit profile</DialogTitle>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">modal content</div>
+            <DialogFooter>
+              <Button type="submit">Save changes</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
       </div>
     </div>
   );
